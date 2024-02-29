0caa14cdca7a059e3d72d623986dd1ed26da9e9e
Youssef Ismail <yismail@ualberta.ca>
2024-02-28 19:08:30 -0700

    Add user json parsing

---
+++ user.py
@@ -0,0 +1,39 @@
+import json
+
+
+class User:
+    def __init__(
+        self, name: str, password: str, joinedGroups=[], ownedGroups=[]
+    ) -> None:
+        self.name: str = name
+        self.password: str = password
+        self.joinedGroups: list[str] = joinedGroups
+        self.ownedGroups: list[str] = ownedGroups
+
+    def __repr__(self) -> str:
+        return f"User(name={self.name}, password={self.password}, joinedGroups={self.joinedGroups}, ownedGroups={self.ownedGroups})"
+
+
+class Users:
+    def __init__(self, jsonPath="users.json"):
+        self.jsonPath = jsonPath
+
+        with open(jsonPath, "r") as f:
+            users = json.load(f)
+
+            self.users = {name: User(name, **user) for name, user in users.items()}
+
+            print("Loaded users from users.json")
+            print(self.users)
+
+    def dump(self):
+        "Dumps users to a file, should be called on exit"
+
+        print("Dumping users to ", self.jsonPath)
+
+    def __del__(self):
+        self.dump()
+
+
+if __name__ == "__main__":
+    users = Users("users.example.json")
