7b1f0f0a96c58a93dcbd1ddb33b89dd720113ef9
Youssef Ismail <yismail@ualberta.ca>
2024-02-28 17:10:42 -0700

    Add Graph Loading from json
    Co-authored-by: Ali1215 <Ali1215@users.noreply.github.com>

---
+++ graph.py
@@ -0,0 +1,55 @@
+import json
+
+
+class Permission:
+    def __init__(self, name, isRead, isWrite) -> None:
+        self.name: str = name
+        self.isRead: bool = isRead
+        self.isWrite: bool = isWrite
+
+    def __repr__(self) -> str:
+        return f"Permission(name={self.name}, isRead={self.isRead}, isWrite={self.isWrite})"
+
+
+class Node:
+
+    def __init__(
+        self, name: str, isFolder: bool, allowedUsers=[], allowedGroups=[], children=[]
+    ) -> None:
+        self.name = name
+        self.children = [Node(**child) for child in children]
+        self.isFolder = isFolder
+        self.allowedUsers: list[Permission] = [
+            Permission(**user) for user in allowedUsers
+        ]
+        self.allowedGroups: list[Permission] = [
+            Permission(**group) for group in allowedGroups
+        ]
+
+    def __repr__(self) -> str:
+        return f"Node(name={self.name}, isFolder={self.isFolder}, allowedUsers={[p.name for p in self.allowedUsers]}, allowedGroups={[p.name for p in self.allowedGroups]}, children={[c.name for c in self.children]})"
+
+
+class Graph:
+    def __init__(self, jsonPath="permissions.json"):
+        self.jsonPath = jsonPath
+
+        with open(jsonPath, "r") as f:
+            graph = json.load(f)
+
+            self.root = Node(**graph)
+
+            print("Loaded graph from permissions.json")
+            print(self.root)
+
+    def dump(self):
+        "Dumps graph to a file, should be called on exit"
+
+        print("Dumping graph to ", self.jsonPath)
+
+    def __del__(self):
+        self.dump()
+
+
+if __name__ == "__main__":
+    graph = Graph("permissions.example.json")
